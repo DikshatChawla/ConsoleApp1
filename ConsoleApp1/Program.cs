fed69c7ebc8b1ccd1ca47f1057543ebde53041d1
visualstudio <visualstudio@122-PC19>
2019-04-17 12:02:49 -0400

    first

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,71 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace FindSummingNumbers
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            new GeneralList().Run(57);
+
+
         }
-    }
-}
+
+
+
+
+        class GeneralList
+        {
+            const int LEN = 100;
+            ArrayList ListQ;
+            int AddUpToNumber = 0;
+            public void Run(int AddUpToNumber)
+
+            {
+
+
+                Random r = new Random();
+
+                ListQ = new ArrayList();
+                for (int i = 0; i < LEN; i++)
+                {
+                    ListQ.Add(r.Next(100));
+
+                }
+                this.findTwoNumbersThatAddUpTo(AddUpToNumber);
+
+
+
+
+            }
+
+            public void findTwoNumbersThatAddUpTo(int number)
+            {
+                foreach (int num2 in ListQ)
+                {
+                    int c, d;
+                    c = num2;
+                    foreach (int num1 in ListQ)
+                    {
+                        d = num1;
+                        if (c + d == 57)
+                        {
+                            Console.WriteLine("The two numbers between 0 and 100 whose sum is 57 are found. Numbers are: " + c + " " + d);
+                        }
+                    }
+
+
+                }
+                Console.ReadLine();
+
+
+            }
+        }
+
+
